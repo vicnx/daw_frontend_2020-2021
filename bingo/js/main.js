e4354f35161a98bace6d40967f7113e9a73c1f18
vicnx <andanivicente@gmail.com>
2020-09-25 10:35:44 +0200

    Añadodo reset Intentear hacerlo en varios archivos webpack css

--- bingo/js/main.js
+++ bingo/js/main.js
@@ -37,14 +37,34 @@ function start(){
     document.getElementById("btn_get").onclick = function(){
         if(bingo.numbers.length<=0){
             document.getElementById("print_number").innerHTML = "NO MORE NUMBERS";
-            document.getElementById("btn_get").innerhtml = "RESET";
+            document.getElementById("btn_get").disabled  = true;
         }else{
+            document.getElementById("btn_get").disabled  = false;
             document.getElementById("btn_get").innerhtml = "Give me another number!";
             document.getElementById("print_number").innerHTML = bingo.getNumber();
             document.getElementById("numbers_deleted").innerHTML = bingo.numbers_deleted;
 
         }
         console.log(bingo.numbers);
+        console.log(bingo.numbers_deleted);
+    }
+
+    document.getElementById("btn_reset").onclick = function(){
+        bingo.numbers=Array.from({length: 99}, (_, i) => i + 1);
+        bingo.numbers_deleted=[];
+        document.getElementById("print_number").innerHTML = "";
+        document.getElementById("numbers_deleted").innerHTML = bingo.numbers_deleted;
+        console.log(bingo.numbers);
+/*         if(bingo.numbers.length<=0){
+            document.getElementById("print_number").innerHTML = "NO MORE NUMBERS";
+            document.getElementById("btn_get").innerhtml = "RESET";
+        }else{
+            document.getElementById("btn_get").innerhtml = "Give me another number!";
+            document.getElementById("print_number").innerHTML = bingo.getNumber();
+            document.getElementById("numbers_deleted").innerHTML = bingo.numbers_deleted;
+
+        } */
+/*         console.log(bingo.numbers); */
         console.log(bingo.numbers_deleted);
     }
 }
