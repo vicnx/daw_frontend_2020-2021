b9e7cd8f3198df79454a85f4a7ae0bca2c419a71
Xente <andanivicente@gmail.com>
2020-10-05 09:21:06 +0200

    Generate card funcionando (falta ordenar card)

--- bingoJS/js/bombo.js
+++ bingoJS/js/bombo.js
@@ -75,17 +75,61 @@ function bingo(){
             // this.insert_forados();
         },
         insert_forados: function(){
+
+            let total =[];
+            let total_sort=[];
+            //function para sacar los que ya tiene 2 forados (comprobacion)
+            function check_dos(total){
+                var repetidos = [];
+                var temporal = [];
+                total.forEach((value,index)=>{
+                    temporal = Object.assign([],total); //Copiado de elemento
+                    temporal.splice(index,1);
+                    if(temporal.indexOf(value)!=-1 && repetidos.indexOf(value)==-1)      repetidos.push(value);
+                });
+                if(repetidos.length >=3){//si ya hay 3 retorna false
+                    total_sort=sort_unique(total);
+                    return false;
+                }else{
+                    return true;
+                }
+            }
+
+            function sort_unique(arr) {
+                if (arr.length === 0) return arr;
+                arr = arr.sort(function (a, b) { return a*1 - b*1; });
+                var ret = [arr[0]];
+                for (var i = 1; i < arr.length; i++) { //Start loop at 1: arr[0] can never be a duplicate
+                  if (arr[i-1] !== arr[i]) {
+                    ret.push(arr[i]);
+                  }
+                }
+                return ret;
+              }
             //las dos primeras lineas con forados
             for (let j = 0; j < 2; j++) {
-                let random_forados=[];
-                while (random_forados.length <4) {
-                    var r = Math.floor(Math.random() * 8) + 1;
-                    if(random_forados.indexOf(r) === -1) random_forados.push(r);
-                }
-                console.log(random_forados)
-                
-                for (let i = 0; i < random_forados.length; i++) {
-                    this.lines[j][random_forados[i]]=null; 
+                let rm_lines_one_two=[];
+                let cont3=true;
+                while (rm_lines_one_two.length <4) {//realiza esto hasta que haya un total de 4 forados
+                    var posicion = Math.floor(Math.random() * 9);
+                    if(!check_dos(total)){
+                        console.log(check_dos(total))
+                        while(total_sort.includes(posicion)){
+                            console.log(total_sort)
+                            console.log("posicion en include: "+posicion)
+                            cont3=false;
+                            posicion = Math.floor(Math.random() * 9);
+                        }
+                    }
+                    if(cont3==true){
+                        if(rm_lines_one_two.indexOf(posicion) === -1){
+                            rm_lines_one_two.push(posicion)
+                            total.push(posicion)
+                        }
+                    }
+                }                
+                for (let i = 0; i < rm_lines_one_two.length; i++) {
+                    this.lines[j][rm_lines_one_two[i]]=null; 
                 }
             }
 
@@ -107,39 +151,54 @@ function bingo(){
                 }
                 
             }
-            console.log(posiciones_con_dos_forados);
 
-            let forados_line_tres=[];
-            while (forados_line_tres.length <4) {
-                let cont_test=true;
-                let r = Math.floor(Math.random() * 8) + 1;
-                //recorremos el carra que tiene guardado las posiciones con mas de dos forados
+
+            let forados_line_tres=[];// declaramos el array donde se van a guardar las posiciones de los forados de la linea 3
+            while (forados_line_tres.length <4) { 
+                let cont3=true;//realizamos todo esto hasta que haya 4 forados.
+                let cont_no_tres_lines=true;//usamos esto contador para indicar que ya se pueden añadir los forados o no
+                let r = Math.floor(Math.random() * 9);
+                //recorremos el array que tiene guardado las posiciones con mas de dos forados
+                console.log(posiciones_con_dos_forados);
                 for (let p = 0; p < posiciones_con_dos_forados.length; p++) {
-                    console.log(posiciones_con_dos_forados);
-                    console.log("ELEMENT: "+posiciones_con_dos_forados[p]+" r: "+r)
-
                     while(posiciones_con_dos_forados[p]==r){
-                        cont_test=false;
-                        // console.log("ELEMENT: "+posiciones_con_dos_forados[p]+" r: "+r)
-                        r=Math.floor(Math.random() * 8) + 1;
+                        cont_no_tres_lines=false;
+                        r=Math.floor(Math.random() * 9);
                     }
                 }
-                if(cont_test==true){
-                    if(forados_line_tres.indexOf(r) === -1) forados_line_tres.push(r);
+                if(cont_no_tres_lines==true){
+                    console.log("check_dos= "+check_dos(total))
+                    if(!check_dos(total)){
+                        // console.log(check_dos(total))
+                        console.log(total_sort);
+                        while(total_sort.includes(r)){
+                            console.log(total_sort)
+                            console.log("r en include: "+r)
+                            cont3=false;
+                            r = Math.floor(Math.random() * 9);
+                        }
+                    }
+                    if(cont3==true){
+                        if(forados_line_tres.indexOf(r) === -1){
+                            forados_line_tres.push(r)
+                            total.push(r)
+                        }
+                    }
+                }
+
+                // console.log(cont_no_tres_lines)
+                console.log(cont3)
+
+                if(cont3==true && cont_no_tres_lines==true){
+                    if(forados_line_tres.indexOf(r) === -1) forados_line_tres.push(r),total.push(r);
                 }
                 
-                console.log(forados_line_tres);
             }
-            // console.log(forados_line_tres)
 
             for (let i = 0; i < forados_line_tres.length; i++) {
                 this.lines[2][forados_line_tres[i]]=null; 
             }
-
-
-            console.log(posiciones_con_dos_forados)
-
-
+            console.log(total);
         },
 
         insert_html: function(){
